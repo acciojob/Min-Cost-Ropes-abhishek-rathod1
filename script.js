144468546d6e1aad02f57599e98b485ee8399b00
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-08 15:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let totalCost = 0;
+	arr.sort((a,b) => a-b);
+
+	while (arr.length >1) {
+		let first = arr.shift;
+		let second = arr.shift;
+
+		let cost = first + second;
+		totalCost += cost;
+
+		arr.push(cost);
+		arr.sort((a,b ) => a-b);
+		
+	}
+	return totalCost;
   
 }
 
